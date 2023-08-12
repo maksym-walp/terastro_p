5f12b0119e65164f263910bfa11266ece9c34efd
maksym-walp <136188072+maksym-walp@users.noreply.github.com>
2023-08-12 18:16:53 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -2,20 +2,25 @@ $(document).ready(function(){
     $('.header_burger').click(function(event){
         $('.header_burger, .header_menu').toggleClass('active');
         $('body').toggleClass('lock');
+        // $('.focus_word').toggleClass('active').detach();
+        // $('.focus_word').appendTo();
     });
-    
 });
+
 
 const slidersOne = document.querySelectorAll(".slider_one .slide");
 const slidersTwo = document.querySelectorAll(".slider_two .slide");
 const slidersTree = document.querySelectorAll(".slider_tree .slide");
 const slidersFour = document.querySelectorAll(".slider_four .slide");
 const slidersFive = document.querySelectorAll(".slider_five .slide");
+const slidersAstrophoto = document.querySelectorAll(".slider_astrophoto .slide");
+// *******************
 let currentSlideOne = 0;
 let currentSlideTwo = 0;
 let currentSlideTree = 0;
 let currentSlideFour = 0;
 let currentSlideFive = 0;
+let currentSlideAstrophoto = 0;
 
 function showSlideOne(slideIndex) {
     slidersOne.forEach((slide) => slide.classList.remove("active"));
@@ -42,6 +47,14 @@ function showSlideFive(slideIndex) {
     slidersFive[slideIndex].classList.add("active");
 }
 
+function showSlideAstrophoto(slideIndex) {
+    slidersAstrophoto.forEach((slide) => slide.classList.remove("active"));
+    slidersAstrophoto[slideIndex].classList.add("active");
+}
+
+
+// **************************************
+
 function nextSlideOne() {
     currentSlideOne = (currentSlideOne + 1) % slidersOne.length;
     showSlideOne(currentSlideOne);
@@ -66,6 +79,14 @@ function nextSlideFive() {
     currentSlideFive = (currentSlideFive + 1) % slidersFive.length;
     showSlideFive(currentSlideFive);
 }
+
+function nextSlideAstrophoto() {
+    currentSlideAstrophoto = (currentSlideAstrophoto + 1) % slidersAstrophoto.length;
+    showSlideAstrophoto(currentSlideAstrophoto);
+}
+
+
+// ********************************
 
 function prevSlideOne() {
     currentSlideOne = (currentSlideOne - 1 + slidersOne.length) % slidersOne.length;
@@ -92,18 +113,28 @@ function prevSlideFive() {
     showSlideFive(currentSlideFive);
 }
 
+function prevSlideAstrophoto() {
+    currentSlideAstrophoto = (currentSlideAstrophoto - 1 + slidersAstrophoto.length) % slidersAstrophoto.length;
+    showSlideAstrophoto(currentSlideAstrophoto);
+}
+
+
+// *************************************
 
 showSlideOne(currentSlideOne);
 showSlideTwo(currentSlideTwo);
 showSlideTree(currentSlideTree);
 showSlideFour(currentSlideFour);
 showSlideFive(currentSlideFive);
+showSlideAstrophoto(currentSlideAstrophoto);
+
+// *****************************************
 
 setInterval(nextSlideOne, 2000);
 setInterval(nextSlideTwo, 2000);
 setInterval(nextSlideTree, 2000);
 setInterval(nextSlideFour, 2000);
 setInterval(nextSlideFive, 2000);
+setInterval(nextSlideAstrophoto, 2000);
 
 ///////////////////////////////////
-
